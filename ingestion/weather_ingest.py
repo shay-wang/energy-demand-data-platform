8a3c01f388b0fb578ca1b05c2f6e584bccab6c1b
Shay Wang <shay.w.data@gmail.com>
2026-03-31 14:19:15 +0900

    feat(pipeline): unify energy and weather ingestion into parallel Airflow DAG
    - Created weather_ingest.py to fetch Open-Meteo data using shared regions.yaml.
    - Refactored DAG to run EIA and Weather extractions as parallel mapped tasks.
    - Standardized both sources to T-3 settlement using UTC-to-Central logic.
    - Implemented BigQuery load tasks for the new weather_raw bronze table.

---
+++ ingestion/weather_ingest.py
@@ -0,0 +1,112 @@
+import os
+import json
+import requests
+from datetime import datetime
+
+
+def fetch_weather_region(
+    region_conf, target_date: datetime, bucket_name=None, client=None, local_path=None
+):
+    """
+    Fetches weather for ONE region's coordinates and stamps it with the Subba ID.
+    """
+    subba = region_conf["id"]
+    lat = region_conf["lat"]
+    lon = region_conf["lon"]
+
+    # Fetch data from Open-Meteo API URL, requesting temperature, humidity, feels like, and wind speed
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "start_date": target_date.strftime("%Y-%m-%d"),
+        "end_date": target_date.strftime("%Y-%m-%d"),
+        "hourly": "temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m",
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "timezone": "UTC",
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    res_json = response.json()
+    actual_lat = res_json.get('latitude')
+    actual_lon = res_json.get('longitude')
+
+    # Convert to NDJSON
+    hourly_data = res_json["hourly"]
+    records = []
+
+    for i in range(len(hourly_data["time"])):
+        record = {
+            "timestamp_utc": hourly_data["time"][i],
+            "subba": subba,                 # add region join key
+            "request_lat": lat,             # lat used for fetching (from YAML)
+            "request_lon": lon,             # lon used for fetching (from YAML)
+            "actual_lat": actual_lat,       # lat returned from API
+            "actual_lon": actual_lon,       # lon returned from API
+            "temp_f": hourly_data["temperature_2m"][i],
+            "humidity_pct": hourly_data["relative_humidity_2m"][i],
+            "apparent_temp_f": hourly_data["apparent_temperature"][i],
+            "wind_speed_mph": hourly_data["wind_speed_10m"][i],
+        }
+        records.append(json.dumps(record))
+
+    ndjson_content = "\n".join(records)
+
+    # Define Hive-style path and file name. (Example: weather/year=2026/month=03/day=02/weather_NCEN.json)
+    file_name = f"weather_{subba}.json"
+    date_path = f"year={target_date.year}/month={target_date.strftime('%m')}/day={target_date.strftime('%d')}"
+
+    # Save Locally (For Testing)
+    if local_path:
+        full_local_dir = os.path.join(local_path, "weather", date_path)
+        os.makedirs(full_local_dir, exist_ok=True)
+        with open(os.path.join(full_local_dir, file_name), "w") as f:
+            f.write(ndjson_content)
+        print(f"Saved locally to {full_local_dir}/{file_name}")
+
+    # Upload to GCS (For Production)
+    if client and bucket_name:
+        gcs_path = f"weather/{date_path}/{file_name}"
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(gcs_path)
+        blob.upload_from_string(ndjson_content, content_type="application/x-ndjson")
+        print(f"Successfully uploaded to gs://{bucket_name}/{gcs_path}")
+
+    return records
+
+if __name__ == "__main__":
+    import yaml
+    from pathlib import Path
+    from google.cloud import storage
+    from google.oauth2 import service_account
+    from dotenv import load_dotenv
+
+    PROJECT_ROOT = Path(__file__).resolve().parents[1]
+    load_dotenv(PROJECT_ROOT / ".env")
+
+    creds_path = os.getenv("INGEST_SA_KEY_PATH")
+    abs_path = PROJECT_ROOT / creds_path
+
+    creds = service_account.Credentials.from_service_account_file(abs_path)
+    client = storage.Client(credentials=creds, project=os.getenv("GCP_PROJECT_ID"))
+
+    bucket_name = os.getenv("GCS_LANDING_BUCKET")
+
+    with open("ingestion/regions.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    # fetch_weather_region(
+    #     region_conf=config['regions'][0],
+    #     target_date=datetime(2026, 2, 10),
+    #     local_path=PROJECT_ROOT / "data"
+    # )
+
+    fetch_weather_region(
+        region_conf=config["regions"][0],
+        target_date=datetime(2026, 2, 12),
+        client=client,
+        bucket_name=bucket_name,
+    )
--- orchestration/dags/eia_weather_dag.py
+++ orchestration/dags/eia_weather_dag.py
@@ -13,20 +13,22 @@ CONFIG_PATH = "/opt/airflow/ingestion/regions.yaml"
 
 
 @dag(
-    dag_id="ext_eia_gcs_to_bq_daily",
-    description="Extract EIA to GCS, then load/transform in BQ.",
+    dag_id="energy_and_weather_daily",
+    description="Extract EIA and Open-Meteo data to GCS, then load/transform in BQ.",
     start_date=pendulum.datetime(2026, 2, 10, tz=local_tz),
     schedule="30 4 * * *",  # Everyday at 4:30 am Central time
     catchup=False,
-    tags=["eia", "daily", "ingestion", "bronze"],
+    tags=["eia", "weather", "ingestion", "bronze"],
     max_active_runs=1,
     default_args={
         "retries": 1,
         "retry_delay": timedelta(minutes=5),
     },
 )
-def eia_pipeline():
-    # Get region list from the yaml config file
+
+def energy_weather_pipeline():
+
+    # -- SETUP TASK --
     @task
     def get_enabled_regions():
         """Reads YAML and returns a list of region dicts."""
@@ -35,16 +37,21 @@ def eia_pipeline():
         # Return only active regions
         return [r for r in config["regions"] if r.get("active", True)]
 
-    # Fetch regional hourly demand data from EIA API and move it to Landing (GCS)
-    @task(task_id="extract_eia_api_to_gcs_raw")
-    def extract_to_gcs(region_config, logical_date):
-        """
-        Run once per region to fetch T-2 settled data from EIA API and saves as NDJSON in GCS Landing.
-        """
-        from ingestion.eia_ingest import fetch_single_region
+    # -- EXTRACTION TASKS (Parallel Dynamic Mapping) --
 
-        # T-2 Logic
-        target_date = logical_date - timedelta(days=3)
+    # Fetch data from EIA API and move it to Landing (GCS)
+    @task(task_id="extract_eia_to_gcs")
+    def extract_eia_task(region_config, data_interval_end):
+        """
+        Run once per region to fetch T-3 settled data from EIA API and save as NDJSON in GCS Landing.
+        """
+
+        from ingestion.eia_ingest import fetch_single_region
+        
+        # T-3 Logic
+        target_date = data_interval_end - timedelta(days=3)
+
+        print(f"Fetching data for target date: {target_date.date()}")
 
         api_key = Variable.get("EIA_API_KEY")
         bucket_name = Variable.get("GCS_LANDING_BUCKET")
@@ -56,14 +63,33 @@ def eia_pipeline():
         fetch_single_region(api_key, region_config, target_date, bucket_name, client)
         return f"eia/year={target_date.year}/month={target_date.strftime('%m')}/day={target_date.strftime('%d')}/"
 
-    # Move data from Landing (GCS) to Bronze (BigQuery)
-    load_raw_to_bronze_table = BigQueryInsertJobOperator(
-        task_id="load_gcs_to_bq_bronze",
+    # Fetch data from Open-Meteo API and move it to Landing (GCS)
+    @task(task_id="extract_weather_to_gcs")
+    def extract_weather_task(region_config, data_interval_end):
+        """
+        Run once per region to fetch T-3 settled data from Open-Meteo API and save as NDJSON in GCS Landing.
+        """
+        from ingestion.weather_ingest import fetch_weather_region
+
+        # Match weather date to EIA data date (T-3)
+        target_date = data_interval_end - timedelta(days=3)
+
+        bucket_name = Variable.get("GCS_LANDING_BUCKET")
+        hook = GCSHook(gcp_conn_id="google_cloud_default")
+        client = hook.get_conn()
+
+        fetch_weather_region(region_config, target_date, bucket_name, client)
+        return f"weather/year={target_date.year}/month={target_date.strftime('%m')}/day={target_date.strftime('%d')}/"
+
+    # --- LOADING TASKS (BigQuery) ---
+    # Move EIA data from Landing (GCS) to Bronze (BigQuery)
+    load_eia_to_bronze = BigQueryInsertJobOperator(
+        task_id="load_eia_to_bq_bronze",
         configuration={
             "load": {
                 "sourceUris": [
                     f"gs://{Variable.get('GCS_LANDING_BUCKET')}/"
-                    + "{{task_instance.xcom_pull(task_ids='extract_eia_api_to_gcs_raw')[0] + '*.json' }}"
+                    + "{{task_instance.xcom_pull(task_ids='extract_eia_to_gcs')[0] + '*.json' }}"
                 ],
                 "destinationTable": {
                     "projectId": "{{ var.value.GCP_PROJECT_ID }}",
@@ -85,10 +111,46 @@ def eia_pipeline():
         gcp_conn_id="google_cloud_default",
     )
 
+    # Move Weather data from Landing (GCS) to Bronze (BigQuery)
+    load_weather_to_bronze = BigQueryInsertJobOperator(
+        task_id="load_weather_to_bq_bronze",
+        configuration={
+            "load": {
+                "sourceUris": [
+                    f"gs://{Variable.get('GCS_LANDING_BUCKET')}/"
+                    + "{{task_instance.xcom_pull(task_ids='extract_weather_to_gcs')[0] + '*.json' }}"
+                ],
+                "destinationTable": {
+                    "projectId": "{{ var.value.GCP_PROJECT_ID }}",
+                    "datasetId": "bronze",
+                    "tableId": "weather_raw",
+                },
+                # Use ingestion time as the time partitioning field
+                "timePartitioning": {"type": "DAY", "field": None},
+                "sourceFormat": "NEWLINE_DELIMITED_JSON",
+                "writeDisposition": "WRITE_APPEND",
+                "autodetect": True,
+                "hivePartitioningOptions": {
+                    "mode": "AUTO",
+                    "sourceUriPrefix": f"gs://{Variable.get('GCS_LANDING_BUCKET')}/weather/",
+                    "requirePartitionFilter": False,
+                },
+            }
+        },
+        gcp_conn_id="google_cloud_default",
+    )
+
+    # --- ORCHESTRATION FLOW ---
+
     regions_list = get_enabled_regions()
-    extract_task = extract_to_gcs.expand(region_config=regions_list)
 
-    extract_task >> load_raw_to_bronze_table
+    # Trigger Dynamic Mapping
+    eia_extractions = extract_eia_task.expand(region_config=regions_list)
+    weather_extractions = extract_weather_task.expand(region_config=regions_list)
+
+    # Set Dependencies
+    eia_extractions >> load_eia_to_bronze
+    weather_extractions >> load_weather_to_bronze
 
 
-eia_pipeline()
+energy_weather_pipeline()
